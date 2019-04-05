012076c9b6407483ab9af773921eebad273fd488
jatinmandav3@gmail.com <jatinmandav3@gmail.com>
2019-04-05 14:06:53 +0530

    Added deep learning based models

---
+++ Models/cnnlstm.py
@@ -0,0 +1,36 @@
+from keras.models import Model
+from keras.layers import Dense, LSTM, Dropout, Input
+from keras.layers import Conv1D, MaxPooling1D, Flatten
+from keras.layers import Activation, RepeatVector, TimeDistributed
+from keras.layers.normalization import BatchNormalization
+
+import keras.backend as K
+
+def CNNLSTMModel(input_shape, output_shape):
+    inp = Input(shape=input_shape)
+
+    x = Conv1D(32, kernel_size=5, padding='same', activation='relu')(inp)
+    x = MaxPooling1D(pool_size=3, strides=2)(x)
+
+    x = Conv1D(64, kernel_size=5, padding='same', activation='relu')(x)
+    x = MaxPooling1D(pool_size=3, strides=2)(x)
+
+    x = Conv1D(128, kernel_size=7, padding='same', activation='relu')(x)
+    x = MaxPooling1D(pool_size=5, strides=2)(x)
+
+    x = Conv1D(256, kernel_size=7, padding='same', activation='relu')(x)
+    x = MaxPooling1D(pool_size=5, strides=2)(x)
+
+
+    x = LSTM(512, return_sequences=True)(x)
+
+    x = Flatten()(x)
+    x = Dense(512, activation='relu')(x)
+    x = Dropout(0.2)(x)
+    x = Dense(512, activation='relu')(x)
+    x = Dropout(0.2)(x)
+
+    out = Dense(output_shape, activation='softmax')(x)
+
+    model = Model(inputs=inp, outputs=out)
+    return model
---
+++ Models/lstm.py
@@ -0,0 +1,24 @@
+from keras.models import Model
+from keras.layers import Dense, LSTM, Dropout, Input
+from keras.layers import Conv2D, MaxPool2D, Flatten
+from keras.layers import Activation, RepeatVector, TimeDistributed
+from keras.layers.normalization import BatchNormalization
+
+import keras.backend as K
+
+def LSTMModel(input_shape, output_shape):
+    inp = Input(shape=input_shape)
+
+    x = LSTM(512)(inp)
+    x = RepeatVector(input_shape[0])(x)
+    x = LSTM(512)(x)
+
+    x = Dense(512, activation='relu')(x)
+    x = Dropout(0.2)(x)
+    x = Dense(512, activation='relu')(x)
+    x = Dropout(0.2)(x)
+
+    out = Dense(output_shape, activation='softmax')(x)
+
+    model = Model(inputs=inp, outputs=out)
+    return model
