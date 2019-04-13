1b87127094758e294fae00583249703cdd697032
jatinmandav3@gmail.com <jatinmandav3@gmail.com>
2019-04-13 12:30:44 +0530

    Minor fixes

--- tf_models/res_lstm.py
+++ tf_models/res_lstm.py
@@ -37,10 +37,10 @@ class ResLSTM:
         print(type(x), type(x[0]))
         print(x[0].get_shape())
 
-        with tf.variable_scope('fw_lstm{}-{}'.format(block_num, 1), reuse=True):
-            forward_lstm1 = rnn.BasicLSTMCell(hidden_1, name='fw_lstm{}-{}'.format(block_num, 1))
-        with tf.variable_scope('bw_lstm{}-{}'.format(block_num, 2), reuse=True):
-            backward_lstm1 = rnn.BasicLSTMCell(hidden_1, name='bw_lstm{}-{}'.format(block_num, 2))
+        with tf.variable_scope('fw_lstm{}'.format(block_num), reuse=True):
+            forward_lstm1 = rnn.BasicLSTMCell(hidden_1, name='fw_lstm{}'.format(block_num))
+        with tf.variable_scope('bw_lstm{}'.format(block_num), reuse=True):
+            backward_lstm1 = rnn.BasicLSTMCell(hidden_1, name='bw_lstm{}'.format(block_num))
 
         rnn_output1, f_states, b_states = tf.nn.static_bidirectional_rnn(forward_lstm1, backward_lstm1,
                                                                          x, dtype=tf.float32)
@@ -49,10 +49,10 @@ class ResLSTM:
         print(type(rnn_output1), type(rnn_output1[0]))
         print(rnn_output1[0].get_shape())
 
-        with tf.variable_scope('fw_lstm{}-{}'.format(block_num+1, 1), reuse=True):
-            forward_lstm2 = rnn.BasicLSTMCell(hidden_2, name='fw_lstm{}-{}'.format(block_num+1, 1))
-        with tf.variable_scope('bw_lstm{}-{}'.format(block_num+1, 2), reuse=True):
-            backward_lstm2 = rnn.BasicLSTMCell(hidden_2, name='bw_lstm{}-{}'.format(block_num+1, 2))
+        with tf.variable_scope('fw_lstm{}'.format(block_num+1), reuse=True):
+            forward_lstm2 = rnn.BasicLSTMCell(hidden_2, name='fw_lstm{}'.format(block_num+1))
+        with tf.variable_scope('bw_lstm{}'.format(block_num+1), reuse=True):
+            backward_lstm2 = rnn.BasicLSTMCell(hidden_2, name='bw_lstm{}'.format(block_num+1))
 
         rnn_output2, f_states, b_states = tf.nn.static_bidirectional_rnn(forward_lstm2, backward_lstm2,
                                                                          rnn_output1, dtype=tf.float32)
@@ -65,7 +65,7 @@ class ResLSTM:
     def model(self, x):
         res1, res2, res_link1 = self.residual_block(x, 1, 16, 32)
         print('-----', res1.get_shape(), res2.get_shape(), res_link1.get_shape())
-        res3, res4, res_link2 = self.residual_block(res_link1, 2, 32, 64)
+        res3, res4, res_link2 = self.residual_block(res_link1, 3, 32, 64)
         print('-----', res3.get_shape(), res4.get_shape(), res_link2.get_shape())
 
         res_link3 = tf.concat([res_link1, res_link2], 2)
