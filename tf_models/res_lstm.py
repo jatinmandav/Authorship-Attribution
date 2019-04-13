a101a99b8ae64476e865ce8d1edc5067dd173f6f
jatinmandav3@gmail.com <jatinmandav3@gmail.com>
2019-04-13 14:08:37 +0530

    Switched from BiLSTM to LSTM in ResLSTM

--- tf_models/res_lstm.py
+++ tf_models/res_lstm.py
@@ -55,27 +55,19 @@ class ResLSTM:
 
     def model(self, x):
         res1, res2, res_link1 = self.residual_block(x, 1, 64)
-        print('-----', res1.get_shape(), res2.get_shape(), res_link1.get_shape())
         res3, res4, res_link2 = self.residual_block(res_link1, 3, 128)
-        print('-----', res3.get_shape(), res4.get_shape(), res_link2.get_shape())
 
         res_link3 = tf.concat([res_link1, res_link2], 2)
-        print(res_link3)
-
         res_link3 = tf.unstack(res_link3, self.timesteps, 1)
 
-        with tf.variable_scope('fw_lstm{}'.format(5), reuse=True):
-            forward_lstm = rnn.BasicLSTMCell(self.hidden_states, name='fw_lstm{}'.format(5))
-        with tf.variable_scope('bw_lstm{}'.format(5), reuse=True):
-            backward_lstm = rnn.BasicLSTMCell(self.hidden_states, name='bw_lstm{}'.format(5))
-
-        reslstm_out, f_states, b_states = tf.nn.static_bidirectional_rnn(forward_lstm, backward_lstm,
-                                                                         res_link3, dtype=tf.float32)
+        with tf.variable_scope('lstm{}'.format(5), reuse=True):
+            lstm5 = rnn.BasicLSTMCell(hidden, forget_bias=1.0, name='lstm{}'.format(5))
+        reslstm_out, states = tf.nn.static_rnn(lstm5, res_link3, dtype=tf.float32)
 
         if not self.attention == None:
             reslstm_out = tf.convert_to_tensor(reslstm_out)
             reslstm_out = tf.tanh(reslstm_out)
-            attention = self.attention.layer1(reslstm_out, 2*self.hidden_states)
+            attention = self.attention.layer1(reslstm_out, self.hidden_states)
             print(attention.get_shape())
             reslstm_out = attention
         else:
@@ -84,7 +76,7 @@ class ResLSTM:
 
         output1 = tf.nn.dropout(reslstm_out, 0.5)
 
-        weights2 = tf.Variable(tf.random_normal([2*self.hidden_states, self.no_classes]))
+        weights2 = tf.Variable(tf.random_normal([self.hidden_states, self.no_classes]))
         biases2 = tf.Variable(tf.random_normal([self.no_classes]))
         output2 = tf.add(tf.matmul(output1, weights2), biases2)
 
